7cd3a306a46bfe7d1d4de95aa7e7e6843ee1a37b
adam-girard <adam@egclimited.com>
2019-10-13 14:49:10 -0400

    This is a new commit for what I originally planned to be amended

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import sys
+import mysql.connector
+import pandas as pd
 
-print(sys.executable)
+con = mysql.connector.connect(
+    user="root", password="!Zinn14113", host="localhost", port="3306", database="egc"
+)
+
+cur = con.cursor()
+cur.execute("SELECT * FROM ve_status WHERE UTILITY='PECO' LIMIT 100")
+
+
+columns = [x[0] for x in cur.description]
+
+df = pd.DataFrame(cur)
+
+cur.close()
+con.close()
+
+
+df.columns = columns
+
+df
+
